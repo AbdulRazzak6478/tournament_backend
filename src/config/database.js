9def1cdf66089f8d8e863ab5a0815f23dd8c1ec2
Abdul Razzak <abdulrazzak74832@gmail.com>
2024-05-24 15:04:16 +0530

    database.js updated

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,33 @@
 const mongoose = require('mongoose');
 const {UR_INDENTIFIER} = require('./server-config')
 
+// async function DBconnect(){
+//     try {
+//         console.log('Connecting to Mongo DB , Please wait .....');
+//         const result = await mongoose.connect(UR_INDENTIFIER)
+//         console.log('Mongo DB connected',UR_INDENTIFIER);
+//     } catch (error) {
+//         console.log('error in connecting to database ,Please check the connection ...');
+//     }
+// }
 async function DBconnect(){
     try {
         console.log('Connecting to Mongo DB , Please wait .....');
-        const result = await mongoose.connect(UR_INDENTIFIER)
-        console.log('Mongo DB connected');
+
+        // console.log('db uri : ',UR_INDENTIFIER);
+        const result = await mongoose.connect(UR_INDENTIFIER);
+        // console.log('db connected : ',result);
+        console.log('connected');
+        const connection = mongoose.connection;
+        connection.on('connected',()=>{
+            console.log('MongoDB connected');
+        });
+        connection.on('error',(err)=>{
+            console.log('MongoDB connection error, Please make sure DB is up and running'+err);
+            process.exit();
+        });
     } catch (error) {
-        console.log('error in connecting to database ,Please check the connection ...');
+        console.log('error in connecting to database ,Please check the connection ...'+error);
     }
 }
 
